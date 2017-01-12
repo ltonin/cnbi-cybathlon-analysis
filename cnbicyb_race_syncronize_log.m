966ee27922b072dd36442f3973e5f9ce4458a2c2
Luca Tonin <luca.tonin@gmail.com>
2017-01-12 11:09:07 +0100

    Added both race and competition synchronization to the same script

--- cnbicyb_race_syncronize_log.m
+++ cnbicyb_race_syncronize_log.m
@@ -5,9 +5,12 @@ subject = 'AN14VE';
 
 experiment  = 'cybathlon';
 datapath    = ['/mnt/data/Research/' experiment '/' subject '/'];
-logpath     = [datapath '/BrainRunnersLogs/v1326/'];
-%logpath     = [datapath '/BrainRunnersLogs/vCompetition/'];
 targetpath  = [datapath '/' subject '_racemat/'];
 
+logpath     = [datapath '/BrainRunnersLogs/v1326/'];
 cnbiutil_bdisp(['[sync] - Syncronizing races between gdf (' datapath ') and logs (' logpath ')']);
+syncGDFlog(subject, datapath, logpath, targetpath)
+
+logpath     = [datapath '/BrainRunnersLogs/vCompetition/'];
+cnbiutil_bdisp(['[sync] - Syncronizing competition races between gdf (' datapath ') and logs (' logpath ')']);
 syncGDFlog(subject, datapath, logpath, targetpath)
