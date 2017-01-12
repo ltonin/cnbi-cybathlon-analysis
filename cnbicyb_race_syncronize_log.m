4a6c465c452b2e88e476160aadb443a69a9907a3
Luca Tonin <luca.tonin@gmail.com>
2017-01-12 15:20:43 +0100

    Added all function to sync log and gdf to one script

--- cnbicyb_race_syncronize_log.m
+++ cnbicyb_race_syncronize_log.m
@@ -7,10 +7,30 @@ experiment  = 'cybathlon';
 datapath    = ['/mnt/data/Research/' experiment '/' subject '/'];
 targetpath  = [datapath '/' subject '_racemat/'];
 
-logpath     = [datapath '/BrainRunnersLogs/v1326/'];
+%% Sync race and gdf
+logpath = [datapath '/BrainRunnersLogs/v1326/'];
 cnbiutil_bdisp(['[sync] - Syncronizing races between gdf (' datapath ') and logs (' logpath ')']);
 syncGDFlog(subject, datapath, logpath, targetpath)
 
-logpath     = [datapath '/BrainRunnersLogs/vCompetition/'];
-cnbiutil_bdisp(['[sync] - Syncronizing competition races between gdf (' datapath ') and logs (' logpath ')']);
-syncGDFlog(subject, datapath, logpath, targetpath)
+%% Sync race time from log (manually for each subject)
+cnbiutil_bdisp('[sync] - Getting only race time from log');
+logpath = [datapath '/BrainRunnersLogs/v1326/'];
+if strcmpi(subject, 'MA25VE')
+    disp('         Date: 20160825');
+    RaceTimeFromLog(subject, '20160825', 1, logpath, targetpath);  % First face off (no gdf data)
+    disp('         Date: 20160922');
+    RaceTimeFromLog(subject, '20160922', 1, logpath, targetpath);  % Second face off (no gdf data)
+elseif strcmpi(subject, 'AN14VE')
+    disp('         Date: 20160825');
+    RaceTimeFromLog(subject, '20160825', 1, logpath, targetpath);  % First face off (no gdf data)
+    disp('         Date: 20161006');
+    RaceTimeFromLog(subject, '20161006', 1, logpath, targetpath);  
+    disp('         RehearsalStyle');
+    findRehearsalStyleRaces(subject, datapath, targetpath)
+end
+
+%% Sync competition data
+logpath_competition = [datapath '/BrainRunnersLogs/vCompetition/'];
+cnbiutil_bdisp(['[sync] - Syncronizing competition races between gdf (' datapath ') and logs (' logpath_competition ')']);
+syncGDFlog(subject, datapath, logpath_competition, targetpath)
+
