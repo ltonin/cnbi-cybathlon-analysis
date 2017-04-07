96ff03257274fc5291a778a7f15ebfca8ac90051
Serafeim Perdikis <serafeim.perdikis@epfl.ch>
2017-04-07 21:52:10 +0200

    Added figure 6 temp, awaiting for Luca's fancy edit

--- modules/cnbicyb_plot_controlparadigm.m
+++ modules/cnbicyb_plot_controlparadigm.m
@@ -28,6 +28,8 @@ for sId = 1:NumSubjects
     cfilepath = [datapath '/' csubject '.time.race.mat'];
     cdatatimerace = load(cfilepath);
     
+    cfilepath = [datapath '/' csubject '.timeonpad.race.mat'];
+    cdatatop = load(cfilepath);    
     
     % Race time, paradigm 3 vs 4
     MeanVecRT = [mean(cdatatimerace.time.run.values(cdatatimerace.time.run.label.Pk==5)) ; mean(cdatatimerace.time.run.values(cdatatimerace.time.run.label.Pk==6))];
@@ -43,8 +45,72 @@ for sId = 1:NumSubjects
         pValPA(t) = ranksum(cdatapadacc.pad.accuracy.run.values(t,cdatapadacc.pad.label.run.Pk==5), cdatapadacc.pad.accuracy.run.values(t,cdatapadacc.pad.label.run.Pk==6));
     end
     
+    % Time on pad, paradigm 3 vs 4
+    IndProt3 = [(find(cdatapadacc.pad.label.run.Pk==5)-1)*18+1 find(cdatapadacc.pad.label.run.Pk==5)*18];
+    IndFProt3 = [];
+    for i=1:size(IndProt3,1)
+        IndFProt3 = [IndFProt3 IndProt3(i,1):IndProt3(i,2)];
+    end
+    IndProt4 = [(find(cdatapadacc.pad.label.run.Pk==6)-1)*18+1 find(cdatapadacc.pad.label.run.Pk==6)*18];
+    IndFProt4 = [];
+    for i=1:size(IndProt4,1)
+        IndFProt4 = [IndFProt4 IndProt4(i,1):IndProt4(i,2)];
+    end    
+    
+    bpdata = [];
+    bplbl = [];
+    xpos = [];
     offset = 0.2;
-    subplot(1,2,1);hold on;
+    for t=1:3
+        %MeanVecTOP(1,t) = median(cdatatop.timeonpad.topall.values(intersect(find(cdatatop.timeonpad.topall.labels==t),IndFProt3)));
+        %MeanVecTOP(1,t) = median(cdatatop.timeonpad.topall.values(intersect(find(cdatatop.timeonpad.topall.labels==t),IndFProt3)));
+        bpdata = [ bpdata ; cdatatop.timeonpad.topall.values(intersect(find(cdatatop.timeonpad.topall.labels==t),IndFProt3))];
+        bplbl = [ bplbl ; (2*(t-1)+1)*ones(length(cdatatop.timeonpad.topall.values(intersect(find(cdatatop.timeonpad.topall.labels==t),IndFProt3))),1)];
+        xpos = [xpos t-offset];
+        bpdata = [ bpdata ; cdatatop.timeonpad.topall.values(intersect(find(cdatatop.timeonpad.topall.labels==t),IndFProt4))];
+        bplbl = [ bplbl ; 2*t*ones(length(cdatatop.timeonpad.topall.values(intersect(find(cdatatop.timeonpad.topall.labels==t),IndFProt4))),1)];        
+        xpos = [xpos t+offset];
+        pValTOP(t) = ranksum(cdatatop.timeonpad.topall.values(intersect(find(cdatatop.timeonpad.topall.labels==t),IndFProt3)),cdatatop.timeonpad.topall.values(intersect(find(cdatatop.timeonpad.topall.labels==t),IndFProt4)));
+    end
+
+    
+    % Time on pad per paradigm
+    subplot(2,2,[1 2]);hold on;
+    hbp = boxplot(bpdata,bplbl,'Positions',xpos,'Colors',[ParadigmColors{1} ; ParadigmColors{2}]);
+    set(hbp,{'linew'},{2});
+    tmppos = cell2mat(get(hbp(1,:),'XData')); % positions of whiskers
+    tmppos = tmppos(:,1)';
+    cellsigstart = {tmppos([1,2]),tmppos([3,4]),tmppos([5,6])};
+    H = sigstar(cellsigstart,pValTOP); 
+    
+    hold off;
+    
+    ylim([0 22]);
+    xlim([tmppos(1)-0.3 tmppos(end)+0.3]);
+    ytick = get(gca, 'YTick');
+    set(gca, 'YTick', intersect(ytick, 1:22));
+    %xticklabel_rotate([],90,[])
+    grid on;
+    
+    hl = findall(gca,'Tag','Box');
+    hl = flipud(hl);
+    hLegend = legend(hl, {'Control paradigm 3', 'Control paradigm 4'});
+    xtick = [1:3] ;
+    set(gca, 'XTick', xtick);
+    set(gca, 'XTickLabel', PadNames);    
+    %hxlabel = xlabel('Command Type');
+    %set(hxlabel, 'Position', get(hxlabel, 'Position') - [0 0.02 0])
+    ylabel('Time [s]');
+    title('Time On Pad');    
+
+    
+    
+    
+    
+    
+    % BCI command accuracy per paradigm
+    offset = 0.2;
+    subplot(2,2,3);hold on;
     cellsigstart = {};
     
     hBar{1} = bar([1:3]-offset,MeanVecPA(1,:),0.25,'LineWidth',2,'FaceColor',ParadigmColors{1});
@@ -69,26 +135,27 @@ for sId = 1:NumSubjects
     grid on;
     
     legend(ParadigmNames, 'location', 'SouthEast');
-    hxlabel = xlabel('Command Type');
-    set(hxlabel, 'Position', get(hxlabel, 'Position') - [0 0.02 0])
+    %hxlabel = xlabel('Command Type');
+    %set(hxlabel, 'Position', get(hxlabel, 'Position') - [0 0.02 0])
     ylabel('Accuracy [%]');
     title('Command Accuracy');
 
     
-    subplot(1,2,2);hold on;
+    % Race time per paradigm
+    subplot(2,2,4);hold on;
     cellsigstart = {};
     
     hBar{1} = bar(1-offset,MeanVecRT(1),0.25,'LineWidth',2,'FaceColor',ParadigmColors{1});
     hBar{2} = bar(1+offset,MeanVecRT(2),0.25,'LineWidth',2,'FaceColor',ParadigmColors{2});
-    hEBar{1} = errorbar(1-offset,MeanVecRT(1),0,StdVecRT(1),'.','LineWidth',2,'Color',ParadigmColors{1});
-    hEBar{2} = errorbar(1+offset,MeanVecRT(2),0,StdVecRT(2),'.','LineWidth',2,'Color',ParadigmColors{2});
+    hEBar{1} = errorbar(1-offset,MeanVecRT(1),0,StdVecRT(1),'.','LineWidth',2,'Color', ParadigmColors{1});
+    hEBar{2} = errorbar(1+offset,MeanVecRT(2),0,StdVecRT(2),'.','LineWidth',2,'Color', ParadigmColors{2});
     
     cellsigstart{1} = [1-offset,1+offset];
     
     H = sigstar(cellsigstart,pValRT); 
     hold off;
     
-    ylim([80 155]);
+    ylim([110 155]);
     xlim([0.5 1.5]);
     ytick = get(gca, 'YTick');
     set(gca, 'YTick', intersect(ytick, 1:240));
